98486be4b16a4274411d41a8d31f2990c10c336b
farhan sharif <farhansharif1410@gmail.com>
2022-10-21 08:00:10 +0530

    news data make

--- index.js
+++ index.js
@@ -4,14 +4,34 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 app.use(cors())
-const categories = require('./data/category.json')
+const categories = require('./data/category.json');
+const news = require('./data/news.json')
+
 
 app.get('/', (req, res) => {
     res.send('This is my first server make.')
 })
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    } else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews)
+    }
+
 })
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
 app.listen(port, () => {
     console.log('Server is Runngin in', port);
 })
